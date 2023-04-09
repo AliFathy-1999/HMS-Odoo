27e6daa6e74f2566c7902babda647373d72c3c44
Ali Ahmed Fathi <72993021+AliFathy-1999@users.noreply.github.com>
2023-04-10 00:35:41 +0200

    Project Done

---
+++ __manifest__.py
@@ -0,0 +1,8 @@
+{
+    'name': 'hms',
+    'author': 'Ali Ahmed',
+    'data': ['views/patient.xml','views/department.xml','views/doctor.xml','reports/hms_templates.xml',
+    'reports/hms_reports.xml','security/hms_security.xml','security/ir.model.access.csv'],
+    'license': 'AGPL-3',
+}
+# 'views/hms_customer.xml'
